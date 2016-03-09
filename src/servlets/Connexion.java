6aa4544d2ec9444e41e02b40dab3bbf7c5a451d8
Yassine Cheqri <cheqriyass@gmail.com>
2016-03-09 14:41:41 +0000

    s<dfasdfsdfsdf

--- src/servlets/Connexion.java
+++ src/servlets/Connexion.java
@@ -22,7 +22,7 @@ public class Connexion extends HttpServlet {
 	public static final String ATT_USER = "utilisateur";
     public static final String ATT_FORM = "form";
     public static final String VUE      = "/index.jsp";
-    public final int x=9;
+    public final int x=10;
     // Injection de notre EJB (Session Bean Stateless)
     @EJB
     private UtilisateurDao   utilisateurDao;
